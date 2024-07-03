dca57da83dca4e6c9e60158799e65e58cef97a26
Wentao Hu <hwt0309@gmail.com>
2024-07-03 15:20:15 +0800

    update

--- utils/dataset_info.py
+++ utils/dataset_info.py
@@ -3,16 +3,14 @@ import os, json
 def get_subjects_prompts_info(info_file_path):
     with open(info_file_path, 'r') as file:
         data_info = json.load(file)["subjects"]
-    
-    prompts = {"prompt_object": data_info["prompt_object"], 
-               "prompt_live": data_info["prompt_live"]}
+        
     subjects = data_info["subject_with_cls"].keys()
     prompts_info = dict()
 
     for subject in subjects:
         if data_info["subject_with_cls"][subject] in ["cat", "dog"]:
-            prompts_info[subject] = {"class": data_info["subject_with_cls"][subject], "prompt_type": "prompt_live"}
+            prompts_info[subject] = {"class": data_info["subject_with_cls"][subject], "prompts": data_info["prompt_live"]}
         else:
-            prompts_info[subject] = {"class": data_info["subject_with_cls"][subject], "prompt_type": "prompt_object"}
+            prompts_info[subject] = {"class": data_info["subject_with_cls"][subject], "prompts": data_info["prompt_object"]}
     
-    return prompts_info, prompts
+    return prompts_info 
